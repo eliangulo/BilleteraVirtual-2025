9d843058ae97a20a17c99a10ab0d977610dc7fbf
Eliana Angulo <eli15angulo@gmail.com>
2025-06-22 22:16:45 -0300

    conectando capas

--- BilleteraVirtual.Server/BilleteraVirtual.Server/obj/Debug/net9.0/BilleteraVirtual.Server.AssemblyInfo.cs
+++ BilleteraVirtual.Server/BilleteraVirtual.Server/obj/Debug/net9.0/BilleteraVirtual.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BilleteraVirtual.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a4512eb730be4239768b3c1623cb6cbfc23c5e6a")]
 [assembly: System.Reflection.AssemblyProductAttribute("BilleteraVirtual.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BilleteraVirtual.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
