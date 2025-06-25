9c84be2b6f8994a7e94ee061779b9d603a10f26d
AgueroJazmin <166668314+AgueroJazmin@users.noreply.github.com>
2025-06-25 00:39:48 -0300

    Tablas Moneda y Deposito (#8)

--- BilleteraVirtual.BD/Datos/Entidades/Moneda.cs
+++ BilleteraVirtual.BD/Datos/Entidades/Moneda.cs
@@ -1,13 +1,31 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace BilleteraVirtual.BD.Datos.Entidades
 { //Jazmin
+
+    [Index(nameof(CodISO), Name = "Moneda_CodISO_UQ", IsUnique = true)]
     public class Moneda : EntityBase
     {
-        public int ID { get; set; }
+
+        [Required(ErrorMessage = "El Tipo de Moneda es requerido")]
+        [MaxLength(3, ErrorMessage = "El Tipo de Moneda tiene como maximo {3} caracteres")]
+        public required string TipoMoneda { get; set; }
+
+        // Habilitada le puse un boolean para que se pueda habilitar o deshabilitar,
+        //No se si les parece bien.
+
+        [Required(ErrorMessage = "La Habilitacion de la moneda es requerido")]
+        public required bool Habilitada { get; set; }
+
+        //
+        [Required(ErrorMessage = "El Codigo ISO es requerido para la representacion de la Moneda")]
+        public int CodISO { get; set; }
+
     }
 }
