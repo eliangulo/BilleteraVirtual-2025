c5f537eaf766e3d2b8d74e05afc980de3f28e380
LucasSalguero <lucassalguero01@gmail.com>
2025-06-24 21:38:11 -0300

    Tablas Usuarios y Billetera

--- BilleteraVirtual.BD/Datos/Entidades/Billetera.cs
+++ BilleteraVirtual.BD/Datos/Entidades/Billetera.cs
@@ -1,13 +1,22 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace BilleteraVirtual.BD.Datos.Entidades
 {//Lucas
+    // Unique
+    
     public class Billetera : EntityBase
     {
-        //public int ID { get; set; }
+        [Required(ErrorMessage = "La Fecha es requerido")]
+        public required DateTime FechaCreacion { get; set; }
+
+        [Required(ErrorMessage = "El rol billetera es requerido")]
+        public required bool Billera_Admin { get; set; }
+
     }
 }
