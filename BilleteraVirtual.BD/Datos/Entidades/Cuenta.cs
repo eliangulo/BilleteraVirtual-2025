adb9b6cbe944e500f8d56f9082fecbf3c130f0bd
LucasSalguero <lucassalguero01@gmail.com>
2025-06-25 01:02:38 -0300

    Thomas Cuenta y extraccion

--- BilleteraVirtual.BD/Datos/Entidades/Cuenta.cs
+++ BilleteraVirtual.BD/Datos/Entidades/Cuenta.cs
@@ -1,13 +1,33 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace BilleteraVirtual.BD.Datos.Entidades
 { //Thomas
+
+    [Index(nameof(Moneda_Tipo), Name = "Cuenta_Moneda_Tipo_UQ", IsUnique = true)]
     public class Cuenta : EntityBase
     {
-        public int ID { get; set; }
+
+        // Clave foranea
+        public required int BilleteraId { get; set; }
+        public Billetera? Billetera { get; set; }
+
+        [Required(ErrorMessage = "La moneda id es requerido")]
+        public required int MonedaId { get; set; }
+        public Moneda? Moneda { get; set; }
+
+
+        [Required(ErrorMessage = "Debe ingresar el tipo moneda")]        
+        public required string Moneda_Tipo { get; set; }
+        [Column(TypeName = "decimal(18, 2)")]
+        public required decimal Saldo { get; set; }
+
+ 
     }
 }
--- BilleteraVirtual.BD/Datos/Entidades/Extraccion.cs
+++ BilleteraVirtual.BD/Datos/Entidades/Extraccion.cs
@@ -1,13 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace BilleteraVirtual.BD.Datos.Entidades
 { //Thomas
+
     public class Extraccion : EntityBase
     {
-        public int ID { get; set; }
+        // Clave foranea
+        public int CuentaID { get; set; }
+        public Cuenta? Cuenta { get; set; }
+
+
+        [Required(ErrorMessage = "El monto es requerido")]
+        [Column(TypeName = "decimal(18, 2)")]
+        public required decimal Monto { get; set; }
+        [Required(ErrorMessage = "El habilitado es requerido")]
+        public required bool Habilitado { get; set; }
+        [Required(ErrorMessage = "La fecha es requerido")]
+        public required DateTime Fecha { get; set; }
+
+
     }
 }
