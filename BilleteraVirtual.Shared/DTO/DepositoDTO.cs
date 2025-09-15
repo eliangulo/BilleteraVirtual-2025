6b069b46d864c9f729a77478983a2a78cc6c15d3
Eliana Angulo Contrera <84999673+eliangulo@users.noreply.github.com>
2025-09-15 19:43:05 -0300

    DTOs, Controllers de Moneda y Deposito (#12)
    Co-authored-by: AgueroJazmin <jazminaguerotejeda@gmail.com>

---
+++ BilleteraVirtual.Shared/DTO/DepositoDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BilleteraVirtual.Shared.DTO
+{
+    public class DepositoDTO
+    {
+        public int Id { get; set; }
+        public int CuentaId { get; set; }
+        public decimal Monto { get; set; }
+        public bool HabilitadO { get; set; }
+        public DateTime Fecha { get; set; }
+
+    }
+}
